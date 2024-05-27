2431d78af8ca70f2c34b3e45ee0dd9fe45dd28da
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-05-27 14:05:43 +0700

    confirm password

--- src/schema/rules.ts
+++ src/schema/rules.ts
@@ -4,6 +4,7 @@ type Rules = {
   [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
   email: {
     required: {
