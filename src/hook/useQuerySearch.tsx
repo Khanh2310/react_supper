98523e6a105477471bf58f49fdac72b7eff9c131
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-08-08 16:53:41 +0700

    update hooks search mutation

--- src/hook/useQuerySearch.tsx
+++ src/hook/useQuerySearch.tsx
@@ -1,3 +1,42 @@
+import { useForm } from 'react-hook-form';
+import { useQueryConfig } from './useQueryConfig';
+import { omit } from 'lodash';
+import { SeacrchInput, SearchInputSchema } from '@/schema/filter/search.type';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createSearchParams, useNavigate } from 'react-router-dom';
+
 export const useQuerySearch = () => {
-  return <div></div>;
+  const queryConfig = useQueryConfig();
+  const { handleSubmit, register } = useForm<SeacrchInput>({
+    defaultValues: {
+      name: '',
+    },
+    resolver: zodResolver(SearchInputSchema),
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmitSearch = handleSubmit((data) => {
+    const config = queryConfig.order
+      ? omit(
+          {
+            ...queryConfig,
+            name: data.name,
+          },
+          ['order', 'sort_by']
+        )
+      : {
+          ...queryConfig,
+          name: data.name,
+        };
+    navigate({
+      pathname: '/',
+      search: createSearchParams(config).toString(),
+    });
+  });
+
+  return {
+    onSubmitSearch,
+    register,
+  };
 };
