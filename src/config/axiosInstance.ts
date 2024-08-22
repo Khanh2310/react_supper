93d69870bebb2b16b886cf5df62368fffdc8c36b
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-08-22 17:01:39 +0700

    add Event Target

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -39,10 +39,11 @@ axiosInstance.interceptors.response.use(
       const message = data.message || error.message;
       toast.error(message);
     }
+
     if (error.response?.status === HttpStatusCode.Unauthorized) {
       removeUserFromLocalStorage();
-      window.location.reload;
     }
+
     return Promise.reject(error);
   }
 );
